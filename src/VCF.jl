f312267e9b5879592e2d36e962e5a6f947616848
Jiahao Chen <jiahao@mit.edu>
2015-07-27 14:45:12 -0400

    Ensure output matrix is sane
    read now produces a genotype matrix where each element records the
    number of variants in that position in that sample, basically counting
    the number of nonzero variant entries over all the ploidies.
    
    Also add a bailout mode to skip non-GT genome-level data, which I don't
    understand right now.

--- src/VCF.jl
+++ src/VCF.jl
@@ -12,15 +12,19 @@ const SLACK_FACTOR = 1.25
 const UPDATE_LINE_INTERVAL = 500
 
 @doc """
-Read VCF files
+Read VCF files into a [0,1,2]-genotype matrix
 
-This is a very simplistic VCF file reader that skips a lot of the data stored in the file.
+Implementation note:
 
-All header lines are ignored.
+    This is a very simplistic VCF file reader that skips a lot of the data
+    stored in the file.
 
-Only field 2 (the chromosomal position) and fields 10+ (the variants) are read in.
+    All header lines are ignored.
 
-The phase of the variant is ignored.
+    Only field 2 (the chromosomal position) and fields 10+ (the variants) are
+    read in.
+
+    The phases of the variants are ignored.
 
 Input:
 
@@ -28,35 +32,57 @@ Input:
 
 Output:
 
-    idxs: Vector of chromosomal positions
-    data: Vector of vectors of variants
-    nnzcols: Vector of nonzero variants at each position
+    Is: Row indexes of variants
+    Js: Column indexes of variants
+    Vs: Number of variants at position I for sample J
 
-Also display progress meter
+Side effects:
+
+    Also displays progress meter
+
+Reference:
+
+    Based on the VCF Version 4.2 Specification at
+
+    http://samtools.github.io/hts-specs/VCFv4.2.pdf
 """ ->
-function read(filename)
-    csize = filesize(filename)
-
-    stream = if endswith(filename, ".gz")
-        GZip.open(filename)
-    else
-        Mmap.mmap(filename)
-    end
-
-    p = Progress(csize, 2, "", 50) #Progress bar
-
-    Is = Int[]
-    Js = Int[]
+function Base.read(filename)
+    Is = Int32[]
+    Js = Int32[]
     Vs = Int8[]
     sizehint!(Is, INITIAL_SIZE_GUESS)
     sizehint!(Js, INITIAL_SIZE_GUESS)
     sizehint!(Vs, INITIAL_SIZE_GUESS)
 
+    csize = filesize(filename)
+    p = Progress(csize, 2, "", 50) #Progress bar
+
     mode = :startline
-    lineno = pos = posstart = rowid = fieldidx = nnzcol = skiplines = 0
+    lineno = pos = posstart = rowid = fieldidx = skiplines = 0
+    thisentry = 0
     colid = 1
     tmpbuf = IOBuffer(19)
     n = 0
+
+    #Now actually try to read file
+    if csize==0
+        warn("File $filename is empty")
+        @goto done
+    end
+
+    stream = if endswith(filename, ".gz")
+        GZip.open(filename)
+    else
+        Mmap.mmap(filename)
+        #except e
+        #    if isa(e, ArgumentError) && e.msg == "requested size must be > 0, got 0"
+        #        warn("File $filename is empty")
+        #        @goto done
+        #    end
+        #end
+    end
+
+    #Iterate through file
     while true
         c = try
             read(stream, UInt8)
@@ -100,8 +126,7 @@ function read(filename)
                 fieldidx += 1
                 if fieldidx == 10
                     mode = :readdata
-                    nnzcol = 0
-                    colid = 0
+                    colid = thisentry = 0
                 end
             end
 
@@ -109,21 +134,48 @@ function read(filename)
             if c == '0' && position(tmpbuf)==0
                 continue
 
-            elseif '0' ≤ c ≤ '9'
-                write(tmpbuf, c)
+            elseif '0' ≤ c ≤ '9' || c == '.'
+                seek(tmpbuf, position(tmpbuf)+1)
+                #write(tmpbuf, c)
 
             elseif c == '\n'
                 rowid += 1
                 mode = :savedata
 
-            elseif (c == '\t' || c == '|' || c == '\\' || c == '\r')
+            elseif (c == '\t' || c == '\r')
                 #Reset IO buffer
+                if position(tmpbuf) > 0
+                    thisentry += 1
+                    seek(tmpbuf, 0)
+                end
                 colid += 1
                 mode = :savedata
+
+            elseif (c == '|' || c == '\\') #Ploidy divider
+                #Reset IO buffer
+                if position(tmpbuf) > 0
+                    thisentry += 1
+                    seek(tmpbuf, 0)
+                end
+                mode = :savedata
+
+            elseif c==':' #Extra genome-level data I don't understand
+                mode = :skipfield
+
             else
-                error("Unknown char $c ($(Uint(c))) on line $lineno, file offset $n`")
+                println(mode)
+                println(takebuf_array(tmpbuf))
+                error("Unknown char $(Char(c)) (0x$(hex(c))) on line $lineno, file offset $n")
+            end
+
+        elseif mode == :skipfield
+            if c == '\t'
+                mode = :readdata
+            elseif c == '\n'
+                mode = :startline
             end
         end
+
         if mode == :savedata
             if lineno % UPDATE_LINE_INTERVAL == 0 #Update progress bar
                 coffset = position(stream, true)
@@ -133,7 +185,7 @@ function read(filename)
             if position(tmpbuf) > 0
                 push!(Is, rowid)
                 push!(Js, colid)
-                push!(Vs, parse(Int8, takebuf_array(tmpbuf)))
+                push!(Vs, thisentry) #parse(Int8, takebuf_array(tmpbuf)))
 
                 if length(Vs) == INITIAL_SIZE_GUESS
                     #More data than initially guessed
@@ -160,11 +212,13 @@ function read(filename)
     println("Actual number of record lines:", lineno-skiplines)
     println("Actual number of nonzeros:", length(Vs))
 
-    nrows = maximum(Is)
-    ncols = maximum(Js)
-    println("Number of rows: ", nrows)
-    println("Number of cols: ", ncols)
-    println("Average density: ", length(Vs)/(nrows*ncols))
+    if length(Vs) > 0
+        nrows = maximum(Is)
+        ncols = maximum(Js)
+        println("Number of rows: ", nrows)
+        println("Number of cols: ", ncols)
+        println("Average density: ", length(Vs)/(nrows*ncols))
+    end
 
     try
         close(stream)
@@ -182,17 +236,31 @@ function Base.parse{T<:Integer}(::Type{T}, s::AbstractArray{UInt8})
 end
 
 #Main driver
-#Read all .vcf.gz files in current directory and saves data as a sparse matrix
-#in coordinate (IJV) format to jld
-for filename in readdir()
-    endswith(filename, ".vcf.gz") || continue
-    destfilename = string(split(filename, ".")[2], ".jld")
+#Read in list of files from command line
+#If no files specified, Read all .vcf.gz and .vcf files in current directory
+#Saves data as a sparse matrix in coordinate (IJV) format to jld
+filelist = length(ARGS) > 0 ? ARGS : filter(f->(endswith(f, ".vcf") ||
+    endswith(f, ".vcf.gz")), readdir())
 
-    isfile(destfilename) && continue
+for filename in filelist
+    destfilename = if count(x->x=='.', filename) > 2
+        #The 1000 genomes project files have names like
+        #ALL.chr9.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz
+        #map to chr9.jld
+        string(split(filename, ".")[2], ".jld")
+    else #Safe choice
+        string(filename, ".jld")
+    end
+
+    #Don't process file if it already exists
+    if isfile(destfilename)
+        warn("Output file $destfilename already exists. Skipping.")
+        continue
+    end
 
     info("Reading $filename:")
 
-    @time (Is, Js, Vs) = readvcf(filename)
+    @time (Is, Js, Vs) = read(filename)
 
     info("Saving sparse matrix to $destfilename:")
     @time JLD.save(destfilename, "I", Is, "J", Js, "V", Vs, compress=true)
